c92028b753d88f76fc7b9bbe246368b78bd83e1a
Vinod <2362857@cognizant.com>
2025-08-05 18:03:45 -0400

    updated course selection functionality and UI

--- SchedulePlanner/src/components/AddCourses.jsx
+++ SchedulePlanner/src/components/AddCourses.jsx
@@ -1,35 +1,26 @@
 import { useState } from 'react';
 import './Courses.css';
 
-export default function CourseList({ courseList, setCourseList, setIsAddCourses }) {
+export default function AddCourses({ courseList, setCourseList, setIsAddCourses }) {
 
     const [selectedCourse, setSelectedCourse] = useState({});
-
-    function handleDelete(id) {
-        const updatedCourseList = courseList.map(course =>
-            course.id === id ? { ...course, isSelected: false } : course
-        );
-        setCourseList(updatedCourseList);
-    }
 
     return (
         <>
             <div className='courseListContainer'>
                 <div className='courseListHeader'>
-                    <h1>Courses</h1>
+                    <h2>Add Courses</h2>
                     <button
                         className='btn btn-light'
-                        onClick={() => setIsAddCourses(true)}
+                        onClick={() => setIsAddCourses(false)}
                     >
-                        <i className="fa fa-plus" aria-hidden="true"></i>
+                        <i className="fa fa-minus" aria-hidden="true"></i>
                     </button>
-
                 </div>
                 <div className="table-responsive">
                     <table className="table table-hover">
                         <tbody>
                             {courseList
-                                .filter(course => course.isSelected)
                                 .map((course) => (
                                     <tr key={course.id}>
                                         <td>
@@ -51,12 +42,15 @@ export default function CourseList({ courseList, setCourseList, setIsAddCourses 
                                         </td>
                                         <td>
                                             <button type="button"
-                                                className="btn btn-outline-danger"
-                                                data-toggle="modal"
-                                                data-target="#deleteCourseModalCenter"
-                                                onClick={() => setSelectedCourse(course)}
+                                                className="btn btn-success"
+                                                onClick={() => {
+                                                    setCourseList(courseList.map(c =>
+                                                        c.id === course.id ? { ...c, isSelected: true } : c
+                                                    ));
+                                                }}
+                                                disabled={course.isSelected}
                                             >
-                                                <i className='fa fa-trash' aria-hidden="true"></i>
+                                                <i className='fa fa-plus' aria-hidden="true"></i>
                                             </button>
                                         </td>
                                     </tr>
@@ -112,53 +106,8 @@ export default function CourseList({ courseList, setCourseList, setIsAddCourses 
                     </div>
                 </div>
             </div>
-
-            {/* Modal to confirm course deletion */}
-            <div className="modal fade" id="deleteCourseModalCenter" tabIndex="-1" role="dialog" aria-labelledby="deleteCourseModalCenter" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="deleteCourseModalLongTitle">
-                                <span
-                                    style={{
-                                        backgroundColor: selectedCourse.color,
-                                        padding: '0 10px',
-                                        borderRadius: '5px',
-                                        display: 'inline-block',
-                                    }}
-                                >
-                                    &nbsp;
-                                </span>&nbsp;
-                                {selectedCourse.name}
-                            </h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <p>Are you sure you want to delete this course?</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button
-                                type="button"
-                                class="btn btn-light"
-                                data-dismiss="modal"
-                                onClick={() => handleDelete(selectedCourse.id)}
-                            >
-                                Yes
-                            </button>
-                            <button
-                                type="button"
-                                class="btn btn-warning"
-                                data-dismiss="modal"
-                            >
-                                No
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </>
-    );
+
+
+    )
 }
